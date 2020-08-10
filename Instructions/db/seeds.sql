38bab1c04969b403d052829fc672371f8afdfd9b
Ken Cheng <kenfcheng@gmail.com>
2020-08-10 11:38:13 -0400

    DB created

---
+++ Instructions/db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers
+    (burger_name, devoured, date)
+VALUES
+    ("Bohemian Radishy", true, "2016-11-05 04:20:00"),
+    ("Gordon Hamsey", false, "2016-11-05 04:20:01"),
+    ("Sweet Home Avocado", false, "2016-11-05 04:20:02"),
+    ("Don't You Four Cheddah 'Bout Me", true, "2016-11-05 16:20:00")
+,
